021289be2b3804e2bf3542d2290717edc7412021
hajin-chung <hajinchung1@gmail.com>
2023-04-05 15:47:59 +0900

    libinput-gestures

--- apply.sh
+++ apply.sh
@@ -1,1 +1,5 @@
-cp -r ./* ~/.config/
+cp -r ./alacritty ~/.config/
+cp -r ./i3 ~/.config/
+cp -r ./i3status ~/.config/
+cp -r ./nvim ~/.config/
+cp ./libinput-gestures/libinput-gestures.conf ~/.config/
