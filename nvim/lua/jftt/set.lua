6354066263592c38771c37c1a168f99cd107f398
hajin-chung <hajinchung1@gmail.com>
2023-04-12 12:39:09 +0900

    use tabs instead of spaces

--- nvim/lua/jftt/set.lua
+++ nvim/lua/jftt/set.lua
@@ -2,10 +2,11 @@ vim.opt.guicursor = ""
 
 vim.opt.nu = true
 
+vim.opt.autoindent = true
+vim.opt.expandtab = false
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+
 
 vim.opt.smartindent = true
 
