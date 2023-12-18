3a1e7013b82fbc296346688655d21c6de74a22f8
hajin-chung <hajinchung1@gmail.com>
2023-12-19 00:17:42 +0900

    update tmux, wezterm configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,5 +17,6 @@ config.window_padding = {
 config.default_prog = { 'wsl' }
 config.adjust_window_size_when_changing_font_size = false
 config.window_background_opacity = 0.9
+config.audible_bell = "Disabled"
 
 return config
