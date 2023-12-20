935b4b9c9ffb2a4e31a7e760460aa083d25c7c96
hajin-chung <hajinchung1@gmail.com>
2023-12-20 22:42:21 +0900

    update neovim buffer bar style

--- init.lua
+++ init.lua
@@ -181,17 +181,50 @@ require('lazy').setup({
   },
 
   {
-    "ojroques/nvim-bufbar",
+    'akinsho/bufferline.nvim',
+    version = "*",
     config = function()
-      require("bufbar").setup {
-        theme = 'nord',   -- the theme in 'lua/bufbar/themes' to use
-        show_tabs = false,    -- show tabs
-        show_bufname = 'all', -- show full buffer name ('current', 'visible' or 'all')
-        show_flags = true,   -- show buffer flags
-        show_alternate = false, -- show alternate buffer
-        modifier = ':t',     -- the name modifier
-        term_modifier = ':t', -- the name modifier for terminal buffers
-        separator = '|',     -- the buffer separator
+      -- vim.opt.termguicolors = true
+      local bufferline = require("bufferline")
+      bufferline.setup {
+        options = {
+          mode = "buffers",                               -- set to "tabs" to only show tabpages instead
+          style_preset = bufferline.style_preset.minimal, -- or bufferline.style_preset.minimal,
+          numbers = "none",
+          close_command = "bdelete! %d",                  -- can be a string | function, | false see "Mouse actions"
+          right_mouse_command = "bdelete! %d",            -- can be a string | function | false, see "Mouse actions"
+          left_mouse_command = "buffer %d",               -- can be a string | function, | false see "Mouse actions"
+          middle_mouse_command = nil,                     -- can be a string | function, | false see "Mouse actions"
+          indicator = {
+            style = 'none',
+          },
+          buffer_close_icon = '󰅖',
+          modified_icon = '●',
+          left_trunc_marker = '<',
+          right_trunc_marker = '>',
+          --- name_formatter can be used to change the buffer's label in the bufferline.
+          --- Please note some names can/will break the
+          --- bufferline so use this at your discretion knowing that it has
+          --- some limitations that will *NOT* be fixed.
+          max_name_length = 18,
+          max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+          truncate_names = true,  -- whether or not tab names should be truncated
+          tab_size = 18,
+          diagnostics_update_in_insert = false,
+          -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
+          color_icons = false,        -- whether or not to add the filetype icon highlights
+          persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
+          move_wraps_at_ends = false, -- whether or not the move command "wraps" at the first or last position
+          -- can also be a table containing 2 custom separators
+          -- [focused and unfocused]. eg: { '|', '|' }
+          separator_style = "thin",
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' }
+          },
+        }
+
       }
     end
   }
