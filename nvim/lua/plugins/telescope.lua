9e7f4e6362125bd8852be66d072e40cffb7e3555
hajin-chung <hajinchung1@gmail.com>
2026-02-23 14:35:04 +0900

    remap telescope move selection keyes

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,7 +6,20 @@ return {
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      local actions = require("telescope.actions")
       require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            },
+            n = {
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
+            }
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown(),
