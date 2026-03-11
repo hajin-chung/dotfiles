95d093f54b2825ba6c2b37ee50994de0d39c2b63
hajin-chung <hajinchung1@gmail.com>
2026-03-11 13:49:40 +0000

    remove last 's' from 'nvim-treesitter.configs'

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
+    main = "nvim-treesitter.config",
     opts = {
       ensure_installed = { "c", "cpp", "lua" },
       auto_install = true,
